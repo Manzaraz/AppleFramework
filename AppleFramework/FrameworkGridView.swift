f4b7c40bbdb84f9c83fdffbdde178b5d8f5d5d2c
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-30 21:39:34 -0300

    Intro & Project Setup

---
+++ AppleFramework/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppleFramework
+//
+//  Created by Christian Manzaraz on 30/07/2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
